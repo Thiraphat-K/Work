1271155f403b88524204f2a8db10184847be65a3
Thiraphat Ketsingnoi <63010467@kmitl.ac.th>
2020-08-30 16:32:27 +0700

    Week3

---
+++ D0.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int b;
+	printf("Show 'KMITL 60 YEAR & CE 59' : 30 Line\n\n");
+	for(b=1;b<=30;b++)
+	{
+		printf("KMITL 60 YEAR & CE 59\tKMITL 60 YEAR & CE 59\n");
+	}
+	return 0;
+}
---
+++ D1.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int Yes,No;
+	printf("DO YOU LOVE ME (Yes/No) : ");
+	scanf("%c", &Yes);
+	scanf("%c", &No);
+	if(Yes)
+	{
+	printf("\nYou're excellent naaaa\n");
+	printf("\nI love you too. krabbbb\n");
+	}
+	else 
+	{
+	printf("\nBadddddddd, Goodbyeeeeeeeeeeeeeee\n");
+	printf("\nI hate You Makkk\n");
+	}
+	return 0;
+}
